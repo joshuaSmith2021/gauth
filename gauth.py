eb64e6b4bd8c434119c3c0e4cc5e1938afed2446
joshuaSmith2021 <joshuasmith6556@gmail.com>
2018-12-28 08:01:08 -0800

    Be rid of pip management stuff, just the library now

---
+++ gauth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+
+import google.oauth2.credentials
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+class OAuth:
+    """
+    Creates an object with multiple methods designed to create and read
+    credentials as well as checking if valid credentials exist. It is
+    initialized by passing in the path to the OAuth client secrets file.
+    """
+    def __init__(self, SECRETS_FILE):
+        self._secrets_file = SECRETS_FILE
+    def create_credentials(self, SCOPES, SERVICE_NAME, VERSION):
+        """
+        Creates OAuth credentials for a user based for a certain API.
+        Returns 'A Resource object with methods for interacting with the service.' -Google Documentation
+        """
+        flow = InstalledAppFlow.from_client_secrets_file(self._secrets_file, SCOPES)
+        credentials = flow.run_console()
+        return build(SERVICE_NAME, VERSION, credentials=credentials)
